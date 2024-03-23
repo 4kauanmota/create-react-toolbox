cddcccfb2c857009d567be68f7b00817672a4a37
Kauan Soares Mota <4kauanmota@gmail.com>
2024-03-23 15:04:08 -0300

    :heavy_minus_sing: build: Removing unused dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  verbose: true,
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleNameMapper: {
     "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
   },
 };
